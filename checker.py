65e39bde1028f50cae7d64705b7f1c78e640844c
mixnr1 <miks.grinbergs@gmail.com>
2020-12-02 16:08:24 +0200

    Added read from file

--- checker.py
+++ checker.py
@@ -1,12 +1,16 @@
 import subprocess
-ip_list = ['google.com', 'tvnet.lv', 'ddfadlkjdklfja.fa']
-for line in ip_list:
-    response=subprocess.Popen(["ping", "-c", "1", line],
+import config
+ip_file = open(config.path_to_file+'missing.txt', 'r')
+for line in ip_file:
+    response=subprocess.Popen(["ping", "-c", "1", line.strip()],
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT)
     stdout, stderr = response.communicate()
     if (response.returncode == 0):
-        status = line + " is Reachable"
-        subprocess.run(["/usr/bin/notify-send", "--icon=error", status])
+        status = line.rstrip() + " is Reachable"
+        # subprocess.run(["/usr/bin/notify-send", "--icon=error", status])
+        subprocess.run(["/usr/bin/notify-send", "--icon=/usr/share/icons/gnome/48x48/status/dialog-warning.png", status])
     else:
+        status = line.rstrip() + " is Not Reachable"
         pass
+    print(status)
