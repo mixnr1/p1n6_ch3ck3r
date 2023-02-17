bda4f449d099fb6d001172d28f326ffb9e7bfb00
mixnr1 <38935530+mixnr1@users.noreply.github.com>
2023-02-17 10:39:29 +0200

    Add files via upload

---
+++ checkerpopup.py
@@ -0,0 +1,22 @@
+import subprocess
+import config
+import time
+import notify2
+notify2.init("My Application")
+ip_file = open(config.path_to_file+'ip_list.txt', 'r')
+for line in ip_file:
+    response=subprocess.Popen(["ping", "-c", "1", line.strip()],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT)
+    stdout, stderr = response.communicate()
+    if (response.returncode == 0):
+        status = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " | " + line.strip() 
+        n = notify2.Notification(
+            "PING SUCCESS NOTIFICATION", 
+            status,
+            "notification-message-im")
+        n.set_timeout(0)
+        n.add_action("close", "Close", lambda *args: None)
+        n.show()
+    else:
+        pass
